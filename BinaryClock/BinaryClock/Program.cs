33eaf9f89293b53133cc90c1daab6615b9e94174
st3chi <148326900+st3chi@users.noreply.github.com>
2024-01-19 22:24:05 +0200

    Add files via upload

---
+++ BinaryClock/BinaryClock/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryClock
+{
+    internal class Program
+    {
+        static string ConvertToBinary(int val)
+        {
+            return Convert.ToString(val, 2).PadLeft(4, '0');
+        }
+
+        static void ConvertTimeToBinary(string timpCurent)
+        {
+            try
+            {
+                string[] timp = timpCurent.Split(':');
+                int ore = int.Parse(timp[0]);
+                int minute = int.Parse(timp[1]);
+                int secunde = int.Parse(timp[2]);
+
+                string oraB = ConvertToBinary(ore);
+                string minutB = ConvertToBinary(minute);
+                string secundaB = ConvertToBinary(secunde);
+
+                Console.WriteLine($"Ceasul binar: {oraB}:{minutB}:{secundaB}");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Formatul orei introdus nu este valid.");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceți ora curentă în formatul HH:MM:SS: ");
+            string timpCurent = Console.ReadLine();
+
+            ConvertTimeToBinary(timpCurent);
+        }
+    }
+}
+
